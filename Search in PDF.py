77b793553a738c0d25dbdb2d696e538bdc5e9155
Creeper798 <espino.con@gmail.com>
2018-04-07 11:43:28 -0600

    Add files via upload

---
+++ Search in PDF.py
@@ -0,0 +1,35 @@
+import PyPDF2, requests
+def main():
+    tries = "yes"
+    url = "https://cleveracademy.vn/wp-content/uploads/2016/10/To-Kill-a-Mockingbird.pdf" #change this to any link later
+    while "y" in tries.lower(): #This keeps the program running as long as someone wants to use it.
+        word, x1 = input("\n\nPlease enter the word you are trying to find: "), 0
+        test(word, x1, url)
+        tries = input("Would you like to search for another word? ") #Do we need this for website???
+def test(word, x1, Q1):
+    x, z = 0, 0
+    transfer = requests.get(Q1)
+    open("PDF.pdf", "wb").write(transfer.content)
+    pdfReader = PyPDF2.PdfFileReader("PDF.pdf")
+    pages = pdfReader.getNumPages()
+    while x1 != pages: #While the number of pages searched is not equal to the number of pages in the PDF: keep searching.
+        derp1 = pdfReader.getPage(x1)
+        derp2 = derp1.extractText()
+        derp2 = derp2.replace("\n", " ") #This takes out PDF formatting of random indents and replaces it with spaces.
+        derp3 = derp2.replace(" ", "") #Taking out all spaces allows for a more accurate search because of PDF formatting.
+        if word.lower() in derp3.lower():
+            z = final(derp3, derp2, word, x, x1, z) #Z is returned from final and printed to show number of results.
+        x1 += 1
+        if not x1 > 0:
+            z = final(derp3, derp2, word, x, x1, z)
+    print(str(z) + " Results found.\n") #This is better than "word not found" because it displays the same information with less code.
+def final(derp3, derp2, word, x, x1, z):
+    nospace = derp3.lower().split(".")
+    derp = derp2.lower().split(".")
+    while x != len(nospace): #This fixes the 1 word per page problem, now it searches through the whole page and returns every instance of the word.
+        if word.lower() in nospace[x]:
+            z += 1 #This allows the variable Z to keep track of how many times a word is found successfully.
+            print('"' + derp[x] + '."  (Found on page: ' + str(x1 + 1) + ")\n\n") #have this output to json or txt file
+        x += 1
+    return z
+main()
